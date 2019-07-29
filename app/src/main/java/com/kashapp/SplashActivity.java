00689b3753d3d0616daf5471950b6d0f646abca0
Muhamad Amir <mmuhamadamirzaidi@gmail.com>
2019-07-29 20:26:34 +0800

    Add Splash Screen

---
+++ app/src/main/java/com/kashapp/SplashActivity.java
@@ -0,0 +1,35 @@
+package com.kashapp;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private ImageView SplashLogo;
+    private int SPLASH_TIME_OUT = 1000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        SplashLogo = (ImageView)findViewById(R.id.splash_logo);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent splash = new Intent(SplashActivity.this, RedirectActivity.class);
+                startActivity(splash);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
+
+        Animation animation = AnimationUtils.loadAnimation(this,R.anim.splashanimation);
+        SplashLogo.startAnimation(animation);
+    }
+}
